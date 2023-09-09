72d236eb52aabaafb7ea16f05d3f813c89da1237
011101110111 <khloudayman780@gmail.com>
2023-09-08 19:33:03 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c ='a';
+	while (c <= 'z')
+	{	
+		putchar(c);
+	        c++;
+	}
+
+	char ch = 'A';
+	while (ch <= 'Z')
+        {
+		putchar(ch);
+		ch++
+	}
+	putchar('\n');
+
+	return (0);
+}
