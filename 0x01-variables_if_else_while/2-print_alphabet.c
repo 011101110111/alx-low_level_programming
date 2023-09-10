ef984c10b960b74de811e82d42a26f7be85e21a8
011101110111 <khloudayman780@gmail.com>
2023-09-10 07:50:29 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,15 @@
 
 int main(void);
 {
-	char c = 'a';
-       	while(c <= 'z')
+	char	c	=	'a';
+        
+	while	(c	<=	'z')
 	{
 		putchar(c);
 		c++;
 	}
- 	putchar('\n');
+ 	
+	putchar	('\n');
 
-	return (0);
+	return	(0);
 }	
