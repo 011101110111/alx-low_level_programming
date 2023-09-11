56db7ae82218ffc765ca55fc38f72ce1f640c382
011101110111 <khloudayman780@gmail.com>
2023-09-11 15:22:43 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ *
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while("%i" digit <= 9)
+        {
+		putchar(digit);
+		digit++;
+	}
+	putchar("\n");
+
+	return (0);
+}
