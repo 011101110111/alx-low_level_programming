d3f6682b38bc10827c1f53b5bad3217812988e4c
011101110111 <khloudayman780@gmail.com>
2023-09-10 20:55:00 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (success)
+ *
+*/
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{ 
+		putcahr(c, != 'q', != 'e' );
+		c++;
+	}
+
+	putchar ('\n');
+
+	Return (0);
+}		
