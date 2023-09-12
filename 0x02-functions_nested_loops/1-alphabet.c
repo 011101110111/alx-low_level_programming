e08ea608447885c1c73113dfd80a861aa9ddf1b5
011101110111 <khloudayman780@gmail.com>
2023-09-12 14:52:24 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,8 @@
 
 
 /**
- * print_alphabet - utilizes in the _putchar function to print the alphabet a-z
+ * print_alphabet -x10 utilizes in the 
+ * 		_putchar function to print the alphabet a-z
 */
 void print_alphabet_x10(void)
 {
