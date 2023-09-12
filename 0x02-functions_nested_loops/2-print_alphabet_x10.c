8e1c4a5720b17c58484309ba2010cdc8c17e12b5
011101110111 <khloudayman780@gmail.com>
2023-09-12 16:02:30 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@
 void print_alphabet_x10(void);
 {
 	int c, ch;
-	
+
 	for (c = 0; c <= 9; c++)
 	{
-		for(ch = 'a'; ch <= 'z'; ch++)
+		for(ch ='a'; ch <='z'; ch++)
 			_putchar(ch);
 
 		_putchar('\n');
