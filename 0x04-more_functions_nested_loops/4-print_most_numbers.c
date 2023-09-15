ac616b67bd00b6b4665ac30540265b85a55b7800
011101110111 <khloudayman780@gmail.com>
2023-09-15 09:36:38 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,13 @@
  */
 void print_most_numbers(void)
 {
-	int num = 0;
 
-	for (num >= 0; num <= 9; num++)
+	for (num = 0; num < 10; num++)
 	{
-		if (num == 2 || num == 4)
-			continue
-			{
-				_putchar(num + 48);
-			}
-	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '');
+		}
+	}
 	_putchar ('\n');
 }
