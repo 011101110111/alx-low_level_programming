7261eb8ae85e601948034f1a0cd9295a4e221774
011101110111 <khloudayman780@gmail.com>
2023-09-15 18:10:23 -0700

    task5 2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,19 @@
  */
 void more_numbers(void)
 {
-	int num, row, count;
+	int row, count;
 
 	for (row = 1; row <= 10; row++)
 	{
 		for (count = 0; count <= 14; count++)
 		{
-			num = count;
-			if (num > 9)
+			if (count > 9)
 
 			{
-				_putchar(1 + 48);
-				num = count % 10;
+				_putchar((count / 10) + 48);
 			}
-			_putchar(num + 48);
+			_putchar((count % 10) + 48);
 		}
 	}
-
 	_putchar ('\n');
 }
