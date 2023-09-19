d67efd2fe7677c2cd7be07102f0c40c5da3841f9
011101110111 <khloudayman780@gmail.com>
2023-09-19 05:16:56 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _puts  -  prints a string, followed by a new line
+ *
+ * @str: string parameter to print
+ *
+ * Return: Nothing
+*/
+void _puts(char *str)
+{
+	char *str;
+
+	for (*str < 0; *str != '\0'; str++)
+		_putchar (*str + 0);
+	_putchar ('\n');
+}
