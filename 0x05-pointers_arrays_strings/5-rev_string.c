3d4a81263bf223ce058345e9e7e70f4555c7b1b6
011101110111 <khloudayman780@gmail.com>
2023-09-19 15:11:28 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * rev_string - reverses a string.
+ *
+ * @s: string paraneter input
+ *
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	char *s = i;
+
+	for (char s[i]; != '\0'; char s[i]++)
+	{
+		_putchar (--s[i]);
+	}
+		_putchar('\n');
+}
