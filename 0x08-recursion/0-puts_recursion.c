c63e98a2920a1b75c8f630680e00d5a4d040758e
011101110111 <khloudayman780@gmail.com>
2023-09-28 19:47:15 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_puts_recursion('\n');
+		_putchar(*s);
+		++s;
+		_puts_recursion(s);
 	}
 	else
-	{
-		_puts_recursion(s + 1);
-	}
-
+	_putchar('\n');
 }
