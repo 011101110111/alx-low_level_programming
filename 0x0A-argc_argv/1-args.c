172e08f63f60ac2f3ab43341ae4c0ab3889dd9b4
011101110111 <khloudayman780@gmail.com>
2023-10-02 15:34:29 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: Argument count
+ * @argv: Array of argument strings
+ *
+ * Return: 0 for succesful exit
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		print("%d\n" argv[i]);
+	}
