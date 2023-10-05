ec247eaba8221726a7b52d516a790aa8590b4a9a
011101110111 <khloudayman780@gmail.com>
2023-10-05 08:49:05 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *s;
 
-	if (size == 0 || s == 0)
+	if (size == 0 || *s == 0)
 		return (0);
 
 	s = malloc(size * sizeof(char));
