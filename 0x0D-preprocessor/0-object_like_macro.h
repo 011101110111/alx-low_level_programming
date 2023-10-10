6761f673aeba109285753def02fffd4fb32ff3f7
011101110111 <khloudayman780@gmail.com>
2023-10-10 13:57:30 -0700

    0-object_like_macro.h:2: ERROR: Macros with complex values should be enclosed in parentheses

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,8 +1,8 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
+#ifndef _OBJECT_LIKE_MACRO.H
+#define _OBJECT_LIKE_MACRO.H
 
 
 #define size 1024
 
 
-#endif 
+#endif /* _OBJECT_LIKE_MACRO.H */
