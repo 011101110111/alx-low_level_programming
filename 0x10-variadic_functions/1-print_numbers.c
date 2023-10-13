1bf5e2240a179515859b66e7cc3700105ca65369
011101110111 <khloudayman780@gmail.com>
2023-10-12 18:26:30 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,13 @@
 #include "variadic_functions.h"
-
+/**
+ *  print_numbers -  prints numbers, followed by a new line with  separator
+ * @separator: string separator
+ * @n: the number of argument
+ * @...: the integer to print
+ *
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list num;
 	unsigned int i;
