3e39e1f72d35b00310a4a7d160e9c019e158de7f
011101110111 <khloudayman780@gmail.com>
2023-10-19 18:09:44 -0700

    tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,15 +8,19 @@
  * @owner: owner's dog
  * Descriptyion: just a long dog struct in a big kitty world
  */
+
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
-typedef struct dog dog_t
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
- dog_t *new_dog(char *name, float age, char *owner);
- void free_dog(dog_t *d);
- #endif
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
