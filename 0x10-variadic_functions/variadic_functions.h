f67ec3dba573498fec39aed12c245f5dcb8d3698
011101110111 <khloudayman780@gmail.com>
2023-10-19 18:27:17 -0700

    task3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,12 +4,27 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct print_form - print all form of variable.
+ *
+ * @c: character.
+ * @f: function.
+*/
+
+typedef struct print_form
+{
+	char *c;
+	void (*f)();
+} print_form_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+void print_char(va_list ap);
+void print_integer(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap);
 
 #endif
