f224ab50c32ae80fbbe3438dac8b3b54590c4689
011101110111 <khloudayman780@gmail.com>
2023-10-24 16:58:39 -0700

    hoop

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list and set the head to NULL.
+ *
+ * @head: linked list.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *nextnode;
+
+	if (head != NULL)
+	{
+
+		nextnode = *head;
+		while (nextnode != NULL)
+		{
+			nextnode = nextnode->next;
+			free(*head);
+			*head = nextnode;
+		}
+	}
+}
