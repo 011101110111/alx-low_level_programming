6a236137fa670236b36c04092c89bbc95715f682
khloud <khloudayman780@gmail.com>
2023-11-27 14:59:12 -0800

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,24 +3,24 @@
 /**
  * read_textfile -  reads a text file and prints it to the POSIX
  * @filename: name of file to read
- * @letters: number of byets to read
+ * @letters: number of bytes to read
  *
- * Return: byets print and read
+ * Return: bytes print and read
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fp;
 	char buf[READ_BUF_SIZE * 8];
-	size_t byets;
+	size_t bytes;
 
 	if (!filename || !letters)
 		return (0);
-	fb = open(filename, O_RDONLY);
+	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 		return (0);
-	byets = read(fp, &buf[0], letters);
-	byets = write(STDOUT_FILENO, &buf[0], byets;
+	bytes = read(fp, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 
 	close(fp);
-	return (byets);
+	return (bytes);
 }
