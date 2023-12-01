4c0fc9fafb83735f05d2366b1a1676c93c477586
khloud <khloudayman780@gmail.com>
2023-12-01 13:08:54 -0800

    g

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,46 +1,38 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strlen - return the lengh of string
- * @s: the string whose lengh
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
  *
- * Return: integer leght of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
-
-
-/**
- * create_file - Create a creates a file.
- * @filename : the file to creat
- * @text_content: text to write
- * Return: return 1 success ,return 0 failure
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t  bytes = 0, len = _strlen(text_content);
+	int nletters;
+	int rwr;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY, O_CREAT, O_TRUNC, S_IRUSR, S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (len)
-		bytes = write(fd, text_content, len);
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
-	return (bytes == len ? 1 : -1);
+
+	return (1);
 }
